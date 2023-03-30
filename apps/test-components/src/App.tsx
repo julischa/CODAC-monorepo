3863eacac3abefa69bc6d99f7bfae4ef43f199bb
OljaGleyzer <o.gleyzer@gmail.com>
2023-03-30 17:07:21 +0200

    fixed index.css

--- apps/test-components/src/App.tsx
+++ apps/test-components/src/App.tsx
@@ -4,7 +4,7 @@ import MainContainer from './components/containers/MainContainer';
 function App() {
   return (
     <MainContainer>
-     <Button bg='bg-dark' text='text-dark'>Add Student</Button>
+      <Button bg='bg-dark' text='text-light'>Add Student</Button>
       <Button>Other Action</Button> 
       </MainContainer>);
 }
